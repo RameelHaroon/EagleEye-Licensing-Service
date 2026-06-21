98277ac631f6a8d719314b577872acb3ccebe914
Rameel Haroon <RameelHaroonOff@gmail.com>
2026-06-21 13:25:57 +0500

    Add licensing resilience configuration

--- src/main/java/com/thoughtmechanix/licensing_service/service/LicenseService.java
+++ src/main/java/com/thoughtmechanix/licensing_service/service/LicenseService.java
@@ -5,10 +5,14 @@ import com.thoughtmechanix.licensing_service.config.ServiceConfig;
 import com.thoughtmechanix.licensing_service.model.License;
 import com.thoughtmechanix.licensing_service.model.Organization;
 import com.thoughtmechanix.licensing_service.repository.LicenseRepository;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -30,9 +34,20 @@ public class LicenseService {
         }
         return license;
     }
-
+    @CircuitBreaker(name = "licenseService", fallbackMethod = "buildFallbackLicenseList")
+    @Bulkhead(name = "licenseService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "buildFallbackLicenseList")
     public List<License> getLicensesByOrg(String organizationId) {
         return licenseRepository.findByOrganizationId(organizationId);
+    }
+
+    private List<License> buildFallbackLicenseList(String organizationId){
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License();
+        license.setLicenseId("0000000-00-00000");
+        license.setOrganizationId( organizationId);
+        license.setProductName("Sorry no licensing information currently available");
+        fallbackList.add(license);
+        return fallbackList;
     }
 
     public void saveLicense(License license) {
